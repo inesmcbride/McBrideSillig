33ba972963a1381b6618154d7da930228349362c
Ines <ines.mcbride@live.com>
2017-10-06 17:37:37 +1300

    added files

--- JS/MS.js
+++ JS/MS.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
-      $('.parallax').parallax();
-    });
+  
+    $('.parallax').parallax();
+    
+    $(".button-collapse").sideNav();
+  
+
+  
+  });
+  
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
+  
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > $(window).height() ) {
+        $("#navigation").addClass("active");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $("#navigation").removeClass("active");
+    }
+});
